d11fdac158d06a5cf61c2cb3847ffb167f21533a
Lucas Zocca <lucaszoccasoares@gmail.com>
2023-11-26 17:50:06 -0300

    feat: adiciona parser, gramatica e erros com setas

--- compilador.py
+++ compilador.py
@@ -1,6 +1,14 @@
 #######################################
-# Erro
+# Imports
 #######################################
+
+from string_com_setas import *
+
+
+#######################################
+# Erros
+#######################################
+
 
 class Error:
     def __init__(self, pos_inicio, pos_fim, nome_erro, detalhes):
@@ -12,12 +20,19 @@ class Error:
     def __str__(self):
         result = f'{self.nome_erro}: {self.detalhes}'
         result += f'Arquivo {self.pos_inicio.arquivo}, linha {self.pos_inicio.linha + 1}'
+        result += '\n\n' + string_com_setas(
+            self.pos_inicio.texto, self.pos_inicio, self.pos_fim)
         return result
 
 
 class CaractereInvalidoError(Error):
     def __init__(self, pos_inicio, pos_fim, detalhes):
         super().__init__(pos_inicio, pos_fim, 'Caractere Inválido', detalhes + '\n')
+
+
+class SintaxeInvalidaError(Error):
+    def __init__(self, pos_inicio, pos_fim, detalhes=''):
+        super().__init__(pos_inicio, pos_fim, 'Sintáxe Inválida', detalhes + '\n')
 
 #######################################
 # Constantes
@@ -39,7 +54,7 @@ class Posicao:
         self.arquivo = arquivo
         self.texto = texto
 
-    def avanca(self, caractere_atual):
+    def avanca(self, caractere_atual=None):
         self.indice += 1
         self.coluna += 1
 
@@ -57,20 +72,29 @@ class Posicao:
 #######################################
 
 
-ML_INT = 'INTEIRO'
-ML_FLOAT = 'FLUTUANTE'
+ML_INT = 'INTE'
+ML_FLOAT = 'FLUT'
 ML_PLUS = 'SOMA'
-ML_MINUS = 'SUBTRAI'
-ML_MUL = 'MULTIPLICA'
-ML_DIV = 'DIVIDE'
-ML_LPAREN = 'ESQUERDA_PARENTESES'
-ML_RPAREN = 'DIREITA_PARENTESES'
+ML_MINUS = 'SUBT'
+ML_MUL = 'MULT'
+ML_DIV = 'DIVI'
+ML_LPAREN = 'ESQD_PAREN'
+ML_RPAREN = 'DIRT_PAREN'
+ML_EOF = 'EOF'
 
 
 class Token:
-    def __init__(self, type, value=None):
+    def __init__(self, type, value=None, pos_inicio=None, pos_fim=None):
         self.type = type
         self.value = value
+
+        if pos_inicio:
+            self.pos_inicio = pos_inicio.copia()
+            self.pos_fim = pos_inicio.copia()
+            self.pos_fim.avanca()
+
+        if pos_fim:
+            self.pos_fim = pos_fim
 
     def __repr__(self):
         if self.value:
@@ -78,7 +102,7 @@ class Token:
         return f'{self.type}'
 
 #######################################
-# Analisador Léxico
+# Lexer - Analisador Léxico
 #######################################
 
 
@@ -104,22 +128,22 @@ class Lexer:
             elif self.caractere_atual in DIGITOS:
                 tokens.append(self.cria_num())
             elif self.caractere_atual == '+':
-                tokens.append(Token(ML_PLUS))
+                tokens.append(Token(ML_PLUS, pos_inicio=self.pos))
                 self.avanca()
             elif self.caractere_atual == '-':
-                tokens.append(Token(ML_MINUS))
+                tokens.append(Token(ML_MINUS, pos_inicio=self.pos))
                 self.avanca()
             elif self.caractere_atual == '*':
-                tokens.append(Token(ML_MUL))
+                tokens.append(Token(ML_MUL, pos_inicio=self.pos))
                 self.avanca()
             elif self.caractere_atual == '/':
-                tokens.append(Token(ML_DIV))
+                tokens.append(Token(ML_DIV, pos_inicio=self.pos))
                 self.avanca()
             elif self.caractere_atual == '(':
-                tokens.append(Token(ML_LPAREN))
+                tokens.append(Token(ML_LPAREN, pos_inicio=self.pos))
                 self.avanca()
             elif self.caractere_atual == ')':
-                tokens.append(Token(ML_RPAREN))
+                tokens.append(Token(ML_RPAREN, pos_inicio=self.pos))
                 self.avanca()
             else:
                 pos_inicio = self.pos.copia()
@@ -127,11 +151,13 @@ class Lexer:
                 self.avanca()
                 return [], CaractereInvalidoError(pos_inicio, self.pos, "'" + caractere + "'")
 
+        tokens.append(Token(ML_EOF, pos_inicio=self.pos))
         return tokens, None
 
     def cria_num(self):
         num_str = ''
         dot_count = 0
+        pos_inicio = self.pos.copia()
 
         while self.caractere_atual != None and self.caractere_atual in DIGITOS + '.':
             if self.caractere_atual == '.':
@@ -144,9 +170,154 @@ class Lexer:
             self.avanca()
 
         if dot_count == 0:
-            return Token(ML_INT, int(num_str))
+            return Token(ML_INT, int(num_str), pos_inicio, self.pos)
         else:
-            return Token(ML_FLOAT, float(num_str))
+            return Token(ML_FLOAT, float(num_str), pos_inicio, self.pos)
+
+#######################################
+# Nós
+#######################################
+
+
+class NumberNode:
+    def __init__(self, token):
+        self.token = token
+
+    def __repr__(self):
+        return f'{self.token}'
+
+
+class BinOpNode:
+    def __init__(self, esquerda, op_token, direita):
+        self.esquerda = esquerda
+        self.op_token = op_token
+        self.direita = direita
+
+    def __repr__(self):
+        return f'({self.esquerda}, {self.op_token}, {self.direita})'
+
+
+class UnOpNode:
+    def __init__(self, op_token, node):
+        self.op_token = op_token
+        self.node = node
+
+    def __repr__(self):
+        return f'({self.op_token}, {self.node})'
+
+
+#######################################
+# Resultado do Analisador Sintático
+#######################################
+
+class ParseResult:
+    def __init__(self):
+        self.error = None
+        self.node = None
+
+    def registro(self, res):
+        if isinstance(res, ParseResult):
+            if res.error:
+                self.error = res.error
+            return res.node
+
+        return res
+
+    def sucesso(self, node):
+        self.node = node
+        return self
+
+    def falha(self, error):
+        self.error = error
+        return self
+
+
+#######################################
+# Parser - Analisador Sintático
+#######################################
+
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.token_indice = -1
+        self.avanca()
+
+    def avanca(self):
+        self.token_indice += 1
+        if self.token_indice < len(self.tokens):
+            self.token_atual = self.tokens[self.token_indice]
+        return self.token_atual
+
+    def parse(self):
+        res = self.expr()
+        if not res.error and self.token_atual.type != ML_EOF:
+            return res.falha(SintaxeInvalidaError(
+                self.token_atual.pos_inicio, self.token_atual.pos_fim,
+                "Esperado '+', '-', '*' ou '/'"
+            ))
+        return res
+
+    #######################################
+
+    def factor(self):
+        res = ParseResult()
+        token = self.token_atual
+
+        if token.type in (ML_PLUS, ML_MINUS):
+            res.registro(self.avanca())
+            factor = res.registro(self.factor())
+            if res.error:
+                return res
+            return res.sucesso(UnOpNode(token, factor))
+
+        elif token.type in (ML_INT, ML_FLOAT):
+            res.registro(self.avanca())
+            return res.sucesso(NumberNode(token))
+        
+        elif token.type == ML_LPAREN:
+            res.registro(self.avanca())
+            expr = res.registro(self.expr())
+            if res.error:
+                return res
+            if self.token_atual.type == ML_RPAREN:
+                res.registro(self.avanca())
+                return res.sucesso(expr)
+            else:
+                return res.falha(SintaxeInvalidaError(
+                    self.token_atual.pos_inicio, self.token_atual.pos_fim,
+                    "Esperado ')'"
+                ))
+
+        return res.falha(SintaxeInvalidaError(
+            token.pos_inicio, token.pos_fim,
+            "Número inteiro ou decimal esperado"
+        ))
+
+    def term(self):
+        return self.bin_op(self.factor, (ML_MUL, ML_DIV))
+
+    def expr(self):
+        return self.bin_op(self.term, (ML_PLUS, ML_MINUS))
+
+    #######################################
+
+    def bin_op(self, func, ops):
+        res = ParseResult()
+        esquerda = res.registro(func())
+        if res.error:
+            return res
+
+        while self.token_atual.type in ops:
+            op_token = self.token_atual
+            res.registro(self.avanca())
+            direita = res.registro(func())
+            if res.error:
+                return res
+            esquerda = BinOpNode(esquerda, op_token, direita)
+
+        return res.sucesso(esquerda)
+
 
 #######################################
 # Executa o código
@@ -154,7 +325,14 @@ class Lexer:
 
 
 def executa(arquivo, texto):
+    # Gera de Tokens
     lexer = Lexer(arquivo, texto)
     tokens, error = lexer.cria_tokens()
+    if error:
+        return None, error
 
-    return tokens, error
+    # Gera Árvore Sintática Abstrata
+    parser = Parser(tokens)
+    arvore_sintatica = parser.parse()
+
+    return arvore_sintatica.node, arvore_sintatica.error
